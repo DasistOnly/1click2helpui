e7323ce2eaa17af0b53be9c59434241784bc5075
Only <47639954+DasistOnly@users.noreply.github.com>
2023-02-03 17:54:36 +0100

    Patched

--- client.lua
+++ client.lua
@@ -1,36 +1,29 @@
 local spawned, waiter, todetect = false, 0, { '<', '>', '.mp4', '.mp3', '.ogg', '.wav', }
 
-function nui(type, msg)
-    SendNUIMessage({ action = type, msg = msg, })
-end
-
 function spawnhelpui(msg)
     if msg == nil then return end
     for i = 1, #todetect do
         if string.find(msg, todetect[i]) then
-            msg = 'Crosside Scripting Detected'
+            msg = ''
         end
     end
-    if msg == 'Crosside Scripting Detected' then
+    if msg == '' then
+        SendNUIMessage({ action = 'closeui' })
         return
     end
     waiter = GetGameTimer()
     if spawned == false then
         spawned = true
-        if string.find(msg, '~') then
-            msg = string.gsub(msg, '~', '')
-        end
-        if string.find(msg, 'INPUT_CONTEXT') then
-            msg = string.gsub(msg, 'INPUT_CONTEXT', 'E')
-        end
-        nui('spawnui', msg)
+        SendNUIMessage({ action = 'spawnui', msg = msg, })
         CreateThread(function()
             local success, result = pcall(function()
             while waiter+100 >= GetGameTimer() do Wait(250) end
             spawned = false
-            Wait(400)
-            if spawned == 'false' then nui('break', nil)
-                end
+            Wait(250)
+            while spawned do
+                Wait(100)
+            end
+                SendNUIMessage({ action = 'closeui' })
             end)
             if not success then return end
         end)
